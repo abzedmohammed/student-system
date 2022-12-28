2ce35c25c39abb3ae45a09e73c250c359a7a21d6
Abzed Mohammed <ibraabzed@gmail.com>
2022-12-28 16:46:53 +0300

    add responsive sidenva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Student Dashboard';
+
+  ngOnInit(): void {
+    $("#toggle-sidenav").on("click", () => {
+      $(".hidden-sidenav").toggleClass("hidden-sidenav-item")
+    })
+
+    $(".sidenav-item").on("mouseenter", () => {
+      $(".hidden-sidenav").removeClass("hidden-sidenav-item")
+    })
+
+    $("#sidenav").on("mouseleave", () => {
+      $(".hidden-sidenav").addClass("hidden-sidenav-item")
+    })
+
+    $(".sidenav-button").on("mouseenter", () => {
+      $(".hidden-sidenav").removeClass("hidden-sidenav-item")
+    })
+
+  }
 }
--- src/app/side-navbar/side-navbar.component.ts
+++ src/app/side-navbar/side-navbar.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-navbar.component.scss']
 })
 export class SideNavbarComponent {
-
+  
 }
